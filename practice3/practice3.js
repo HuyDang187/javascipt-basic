ccd094da6d3922ecee837a4f9475065f8f16c321
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-07-27 18:56:59 +0700

    practice 4

--- practice3/practice3.js
+++ practice3/practice3.js
@@ -1,3 +1,5 @@
 let c = 5;
 let d = 4.9;
 console.log('c + d : ',(c+d))
+
+console.log('test string :'+' c= '+c +' d= '+d+' c+d= '+(c+d))
---
+++ practice4/practice4.js
@@ -0,0 +1,6 @@
+let a = 'Toi la String - chuoi';
+let text = `We are the so-call 
+
+"Vikings" from the north`;
+console.log(text);
+console.log(a);
