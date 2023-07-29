3f2f3c964d62cb4459f471046725c90393e01a5e
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-07-29 10:36:16 +0700

    practice 8

---
+++ practice8/practice8.js
@@ -0,0 +1,17 @@
+console.log('Hello World from HTML ')
+
+let a = {};
+let b = 'Huy'
+
+console.log(`Type of a: `,typeof a,'Type of b: ',typeof b);
+
+let obj = {
+    name:'Huy',
+    address:'Ha Noi'
+}
+
+console.log(`what is your name ? `,`My name is `,obj.name)
+
+console.log(`Where are you from ? `, `I'am from `, obj['address'])
+
+console.log(`object is : `,obj)
