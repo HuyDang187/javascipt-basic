9ca6b084085ad63a3a3de288b9424fd80d901e24
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-07-29 11:43:10 +0700

    pactice10

---
+++ practice10/practice10.js
@@ -0,0 +1,14 @@
+console.log('Hello World from HTML')
+
+let a = 5;
+let b = '5';
+
+console.log(`compare a==b `,`a= ` ,a, `b= `, b,`a==b : `,a==b)
+console.log(`compare a===b `,`a= ` ,a, `b= `, b,`a===b : `,a===b)
+
+let c ='';//empty
+let d = null; // null
+let e;//underfined
+console.log(`check value c : `, c)
+console.log(`check value c : `, d)
+console.log(`check value c : `, e)
