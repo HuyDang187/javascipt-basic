c70c7eda1b12e815c569feebd332173dc7b70635
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-07-29 11:08:51 +0700

    practice 9

---
+++ practice 9/practice9.js
@@ -0,0 +1,9 @@
+console.log('Hello World from HTML')
+
+let b = ['item1','item2','item3']
+
+console.log(`Array b : `,b,`type of b: `,typeof b,`first array : `,b[0])
+
+let a = {name:'Huy',address:'Hn'}
+let family = [{name:'Huy',address:'Hn'},{name:'Ha',address:'Lc'}]
+console.log(`type of family: `,typeof family, `Family is: `,family,`the first one: `,family[0] , `name of first one : `,family[0]['name']) 
