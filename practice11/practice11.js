c08a8798a24111b15fa190981943c35cba23a2cc
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-07-31 11:33:19 +0700

    practice11

---
+++ practice11/practice11.js
@@ -0,0 +1,12 @@
+console.log('Hello world from HTML')
+
+
+for(let i = 0 ; i < 10 ; i++){
+    console.log(`value of i = : `,i)
+}
+
+let animal = ['Dog','cat','butterfly','bird','fish']
+
+for(let e =0 ; e < animal.length;e++){
+    console.log(`animal is : `,animal[e],` e= `,e)
+}
