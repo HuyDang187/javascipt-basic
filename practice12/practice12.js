117105ccda6043f6d94e757448f7d2f6b07b11e7
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-07-31 12:46:08 +0700

    practice 12-13

---
+++ practice12/practice12.js
@@ -0,0 +1,14 @@
+let animal = ['Dog' , ' Cat','fish','butterfly','bull']
+
+
+let i = 0 
+
+while(i<animal.length){
+    console.log(`animal `,(i+1),` is `,animal[i])
+    i++;
+}
+
+// do{
+//     console.log(`animal `,(i+1),` is `,animal[i])
+//     i++;
+// }while(i<animal.length)
