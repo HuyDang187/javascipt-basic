d323e7fddd129d07d886a8d9ed3b224240a6c89b
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2023-08-01 15:08:21 +0700

    practice16

---
+++ practice16/practice16.js
@@ -0,0 +1,17 @@
+console.log(`Hello world from HTML `)
+
+
+// function
+
+function sum(x,y){
+    return x+y;
+}
+
+//arrow function
+let sumarrow = (x,y) =>{
+    return x+y;
+}
+
+console.log(`check sum (function): ` ,sum(6,9))
+
+console.log(`check sum (arrow function): `,sumarrow(6,9))
